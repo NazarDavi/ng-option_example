861bb5e4e58390d199769abd16d8806da2cf6f6a
gitVn <islam.vindizhev@csssr.com>
2015-11-10 18:16:36 +0300

    ng-repeat with options replaced by ng-options

--- app.js
+++ app.js
@@ -43,48 +43,4 @@ app.controller('exampleController', function($scope) {
 				]
 		},
 	];
-
-	$scope.cages = [];
-
-	angular.forEach($scope.volaries, function(volary) {
-		angular.forEach(volary.cages, function(cage) {
-			if (cage.volary_id = volary.volary_id) {
-				$scope.cages.push(cage);				//Собираем массив всех клеток
-			}
-		});
-	});
-
-	$scope.initVolary = function() {					//Если ранее вольер был выбран модель инициализируется этим значением, иначе - значением по умолчанию
-		var init;										//Для того, чтобы был выбран соответствующий option возвращается значение init
-
-		if ($scope.request.volary_id) {
-			init = $scope.request.volary_id;
-		} else {
-			angular.forEach($scope.volaries, function(volary) {
-				if (volary.is_default === 1) {
-					init = volary.volary_id;
-					$scope.request.volary_id = volary.volary_id;
-				}
-			});
-		}
-
-		return init;
-	};
-
-	$scope.initCage = function() {						//Если ранее была выбрана клетка модель инициализируется этим значением, иначе - значением по умолчанию
-		var init;										//Для того, чтобы был выбран соответствующий option возвращается значение init
-
-		if ($scope.request.cage_id) {
-			init = $scope.request.cage_id;
-		} else {
-			angular.forEach($scope.cages, function(cage) {
-				if (cage.is_default === 1 && cage.volary_id === $scope.request.volary_id) {
-					init = cage.cage_id;
-					$scope.request.cage_id = cage.cage_id;
-				}
-			});
-		}
-
-		return init;
-	};
 });
